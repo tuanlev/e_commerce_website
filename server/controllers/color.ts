dfa0ff56691f9618d5f92233fdde73882a76cfe4
tuanle13-git <tuanlv1303@gmail.com>
2025-04-25 16:37:52 +0700

    add colorController

---
+++ server/controllers/color.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express"
+import ColorModel from "../models/color";
+import { successResponse } from "../utils/reponseHandle";
+
+export const createColor = async (req: Request, res: Response,next:NextFunction) => {
+    try {
+        const {hexCode} = req.body;
+        const colorIntance = await ColorModel.create({
+            hexCode
+        });    
+    successResponse({res, status:201, message:"Color created successfully", data: colorIntance});
+    } 
+    catch (error) {
+        next(error);
+    }
+}
+export const getColorById = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const colorId = req.params.color_id;
+        const colorResult = await ColorModel.findById(colorId);
+        if (!colorResult) throw new Error("Color not found");        
+        successResponse({res, status:200, message:"Color found", data: colorResult});
+    } catch (error) {
+        next(error);
+    }
+}
+export const getAllColors = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const colors = await ColorModel.find();
+        if (colors?.length === 0) throw new Error("No colors found");
+        successResponse({res, status:200, message:"Colors found", data: colors});
+    } catch (error) {
+        next(error);
+    }
+}
